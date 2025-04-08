2f8464f38ac3d80dc119df68329f03c3eacc195a
Santosh Kumar Guntupalli <santoshkumarguntupalli@gmail.com>
2025-04-08 15:46:25 +0100

    Data transformation completed

--- books_recommender/entity/config_entity.py
+++ books_recommender/entity/config_entity.py
@@ -9,4 +9,7 @@ DataValidationConfig = namedtuple("DataValidationConfig", ["clean_data_dir",
                                                          "ratings_csv_file",
                                                          "serialized_objects_dir"]) 
 
+DataTransformationConfig = namedtuple("DataTransformationConfig", ["clean_data_file_path",
+                                                                   "transformed_data_dir"]) 
+
 
