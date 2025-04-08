f320435295483c5f6d668d5f8c66972a66dbce8b
Santosh Kumar Guntupalli <santoshkumarguntupalli@gmail.com>
2025-04-08 16:02:26 +0100

    Model training completed

--- books_recommender/entity/config_entity.py
+++ books_recommender/entity/config_entity.py
@@ -13,3 +13,8 @@ DataTransformationConfig = namedtuple("DataTransformationConfig", ["clean_data_f
                                                                    "transformed_data_dir"]) 
 
 
+ModelTrainerConfig = namedtuple("ModelTrainerConfig", ["transformed_data_file_dir",
+                                                      "trained_model_dir",
+                                                      "trained_model_name"])
+
+
